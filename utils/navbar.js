4573f755cc75fff7ae7f3576af693e68150c98cb
lorikramosaj <lorikramosaj08@gmail.com>
2022-12-23 23:27:38 +0100

    Added footer and navbar as js components

---
+++ utils/navbar.js
@@ -0,0 +1,17 @@
+var navbar = `<a href="index.html" id="index-link" style="text-decoration: none;"><h1 id="header-title">Hunger <span>Hunt</span></h1></a>
+                <ul class="page-links">
+                    <a href="index.html" id="index-link" style="color: rgb(197, 138, 27)">HOME</a>
+                    <a href="reservations.html" id="reservations-link">RESERVATIONS</a>
+                    <a href="menu.html" id="menu-link">MENU</a>
+                    <a href="blog.html" id="blog-link">BLOG</a>
+                    <a href="shop.html" id="shop-link">SHOP</a>
+                    <a href="contact.html" id="contact-link">CONTACT</a>
+                </ul>
+                <ul class="social-links">
+                    <a href="https://www.facebook.com" id="facebook-link"><i class="fa fa-facebook icon" style="color: white;"></i></a>
+                    <a href="https://www.twitter.com" id="twitter-link"><i class="fa fa-twitter icon" style="color: white;"></i></a>
+                    <a href="https://www.instagram.com" id="instagram-link"><i class="fa fa-instagram icon" style="color: white;"></i></a>
+                </ul>
+`;
+
+document.querySelector('nav').innerHTML = navbar

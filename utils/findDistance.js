65f4cb5bab8f0fb6d3eb96c821dce1b940892287
auritabytyci <auritabytyqi2002@gmail.com>
2023-01-01 15:32:12 +0100

    Finished findDistance component functionality and implemented in UI

--- utils/findDistance.js
+++ utils/findDistance.js
@@ -1,21 +1,52 @@
-const worker = new Worker('worker.js');
+const restauratnLatitude = 40.752440;
+const restauratnLongitude = -73.973780;
+var userLatitude;
+var userLongitude;
 
-navigator.geolocation.getCurrentPosition(position => {
-  const userLatitude = position.coords.latitude;
-  const userLongitude = position.coords.longitude;
-});
+function findDistance() {
+   if(userLatitude === undefined || userLongitude === undefined){
+     alert("User coordinates not found, please try again!")
+   }
+   else {
+    const distance = Math.round(getDistanceFromLatLonInKm(userLatitude, userLongitude, restauratnLatitude, restauratnLongitude))
+    const showDistance = document.getElementById("distance-calculator")
+    showDistance.innerHTML = "<h5>Distance to the restaurant is " + distance+" km</h5>"
+   }
+   
+}
 
-const pointLatitude = 40.752440;
-const pointLongitude = -73.973780;
+if (!navigator.geolocation) {
+  alert('Geolocation is not supported by your browser');
+} else {
+  navigator.geolocation.getCurrentPosition(success, error);
+}
 
-worker.postMessage({
-  userLatitude,
-  userLongitude,
-  pointLatitude,
-  pointLongitude
-});
+function success(position) {
+  userLatitude = position.coords.latitude;
+  userLongitude = position.coords.longitude;
+}
 
-worker.addEventListener('message', event => {
-  const distance = event.data;
-  document.querySelector('#distance').innerHTML = `Distance from your location to the specified point is ${distance}`;
-});
+function error() {
+  console.log('Unable to retrieve your location');
+}
+
+// Haversine formula
+function getDistanceFromLatLonInKm(userLatitude, userLongitude, restaurantLatitude, restaurantLongitude) {
+  var R = 6371;
+  var dLat = deg2rad(restaurantLatitude - userLatitude);
+  var dLon = deg2rad(restaurantLongitude - userLongitude);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(userLatitude)) * Math.cos(deg2rad(restaurantLatitude)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180)
+}
+
+document.querySelector('#find-distance').addEventListener('click', findDistance);

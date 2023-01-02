73f8fea016a5877a546a1d46db49e33c76e590d6
lorikramosaj <lorikramosaj08@gmail.com>
2023-01-03 00:35:19 +0100

    Added while

--- utils/blogComments.js
+++ utils/blogComments.js
@@ -45,14 +45,16 @@ for (let blog = 0; blog < 12; blog++) {
     blogs[blog] = document.getElementById(`blog-comments-${blog+1}`)
     console.log(blogs)
     var commentsContent= '';
-    for (let i = 0; i < 3; i++) {
+    let i = 0;
+    while (i < 3) {
         commentsContent = commentsContent + '<div style="display:flex; flex-wrap: wrap; ">' +
                               '<div style="font-size: 25px; margin: 0px 5px 0px 6px; color: white; width: 10%; height: 40px; background-color: #626258 ; border-radius: 20px"><i class="fa fa-user" style="padding-top: 15%; padding-left: 25%;"></i></div>' +
                               '<div style = "margin: 0px 10px 13px 0px; border-radius: 15px; width: 82%; background-color: rgba(197, 138, 27, 0.1);   border: 0.8px solid rgb(197, 138, 27); font-family: sans serif;">' +
                                      '<span style="font-size: 13px; font-weight: 600; color: black; padding-left: 12px; padding-top: 3px;">' + peopleNames[blog*2+i] + '</span>' +
                                      '<p style="font-size: 11px; color: #23231F; padding-left: 20px; padding-bottoms: 5px;">' + commentExamples[blog*2+i] + '</p>' +
                               '</div>' +
-                          '</div>' 
+                          '</div>' ;
+        i++;
      }
      blogs[blog].innerHTML = commentsContent
 }

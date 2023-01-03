b97e8ee53947176951af40d17a1b45893120daa3
wictro <viktor@frakton.com>
2023-01-03 20:46:42 +0100

    Fixed login validation bug

--- forms/validate-login.js
+++ forms/validate-login.js
@@ -3,7 +3,7 @@ let EMAIL_REGEXP = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 let PASSWORD_REGEXP = /^[a-zA-Z0-9.,@!&$]{8,}$/;
 
 let emailInput = document.getElementById("email");
-let passwordInput = document.getElementById("passowrd");
+let passwordInput = document.getElementById("password");
 
 let dataset = Object.values(window.localStorage).map(item => JSON.parse(item));
 console.log(dataset);
@@ -12,6 +12,7 @@ form.onsubmit = (event) => {
   let isValidEmail = EMAIL_REGEXP.test(emailInput.value);
   let isValidPassword = PASSWORD_REGEXP.test(passwordInput.value);
 
+  console.log("aa");
   if (!isValidEmail || !isValidPassword) {
     alert("Please fill in all necessary fields with appropriate standard");
     event.preventDefault();
@@ -20,11 +21,9 @@ form.onsubmit = (event) => {
         let email = emailInput.value;
         let password = passwordInput.value;
 
-        const user = dataset.find((item) => {
-            return item.email === email;
-        });
+        const user = dataset.find(item => item.email === email);
 
-        if(user == null){
+        if(!user){
             alert("An account with the given email address doesn't exist. Please create one!");
             event.preventDefault();
             return;
@@ -36,6 +35,8 @@ form.onsubmit = (event) => {
             return;
         }
 
+        event.preventDefault();
+        
         alert('Successful login!');
         window.location.href = "../index.html"
   }
